7b5c4a2e233662086beccfe0e11fbdf4994905cc
wx <xing.wang@xforwardai.com>
2024-03-12 16:52:33 +0800

    feat: 支持 通过 pnpm run  commit 提交代码

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-function add(){
-  console.log("测试格式化代码")
+function add() {
+  console.log('测试格式化代码1')
 }
 
 add()
